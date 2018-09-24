15da7c08f9c10e951a40c9ed8579f73c74aa1529
Sman13 <kuttison2@gmail.com>
2018-09-24 07:32:47 -0400

    added favourites column for starred items

--- apiScripts.js
+++ apiScripts.js
@@ -19,10 +19,10 @@ function starRepo (repo) {
 	 }
  }
 }
-function unstarRepo (repo) {
+function starredRepo (repo, row) {
  var Url = "https://api.github.com/user/starred/" + repo;
  var xhr = new XMLHttpRequest();
-			  xhr.open('Delete', Url, true);
+			  xhr.open('Get', Url, true);
 			 xhr.setRequestHeader( 'Content-Type',   'application/json' );
  
 	xhr.setRequestHeader("Authorization", "token " + apiKey); 	
@@ -31,8 +31,7 @@ function unstarRepo (repo) {
 	
 	function processRequest(e) {
 	 if (xhr.readyState == 4 && xhr.status == 204) {
-		var response1 = "unstarred";
-		document.getElementById("origin").innerHTML = response1;
+		document.getElementById("favourites").innerHTML += row;
 	 }
  }
 }
@@ -92,10 +91,12 @@ function tags (tagUrl, tagNumber) {
 		 if (jQuery.isEmptyObject(tags))
 		 {
 			 document.getElementById("tag" + tagNumber).innerHTML = "-";
+			 document.getElementById("tags" + tagNumber).innerHTML = "-";
 		 }
 		 else
 		 {
 			 document.getElementById("tag" + tagNumber).innerHTML = tags[0].name;
+			 document.getElementById("tags" + tagNumber).innerHTML = tags[0].name;
 		 }
 	 }
  };
@@ -104,37 +105,10 @@ function tags (tagUrl, tagNumber) {
 	
 	
 }
-function favourites (tagUrl, tagNumber) {
-  	var Url = tagUrl;
- 	var xhr = new XMLHttpRequest();
-			  xhr.open('Get', Url, true);
-			 xhr.setRequestHeader( 'Content-Type',   'application/json' );
- 
-	xhr.setRequestHeader("Authorization", "token " + apiKey); 	
- 	xhr.send();
- 	xhr.onreadystatechange = processRequest;
-	 function processRequest(e) {
-	 if (xhr.readyState == 4 && xhr.status == 200) {
-		 var tags = JSON.parse(xhr.responseText);
-		 
-		
-		 if (jQuery.isEmptyObject(tags))
-		 {
-			 document.getElementById("tag" + tagNumber).innerHTML = "-";
-		 }
-		 else
-		 {
-			 document.getElementById("tag" + tagNumber).innerHTML = tags[0].name;
-		 }
-	 }
- };
-	
-	//results = JSON.parse(xhr.responseText);
-	
-	
-}
+
 function search (searchParam) {
 	$("#results").load("LeftTableHeader.html");
+	$("#favourites").load("LeftTableHeader.html");
 	searchParam = searchParam.replace(/ /g,'+');
  var Url = "https://api.github.com/search/repositories?q=" + searchParam;
 	
@@ -160,14 +134,25 @@ function search (searchParam) {
 function MakeLeftTable (searchResults) {
 	//$("#results").load("LeftTableHeader.html");
 	var table = document.getElementById("results").innerHTML;
-	table += "<tr>";
-	for (var i=0; i<10; i++) {
+	var row = ""; 
+	
+	for (var i=0; i<10 && !(jQuery.isEmptyObject(searchResults.items[i])); i++) {
+		row="";
+		table += "<tr>";
 	table += "<td>" + searchResults.items[i].full_name + "</td>";
 		//var language = mainLanguage(searchResults.items[i].full_name);
 		table += "<td>" + searchResults.items[i].language + "</td>";
 		table += "<td id=\"tag" + i + "\"></td>";
 		table += "<td id=\"favourite" + i + "\"></td>";
-	table += "</tr>";
+		table += "</tr>";
+		row += "<tr>";
+	row += "<td>" + searchResults.items[i].full_name + "</td>";
+		//var language = mainLanguage(searchResults.items[i].full_name);
+		row += "<td>" + searchResults.items[i].language + "</td>";
+		row += "<td id=\"tags" + i + "\"></td>";
+		row += "<td id=\"favourites" + i + "\"></td>";
+	row += "</tr>";
+		starredRepo(searchResults.items[i].full_name,row);
 	}
 	document.getElementById("results").innerHTML=table;
 	for (var i=0; i<10; i++) {
